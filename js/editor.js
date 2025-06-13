e2b209c143e4f67f3f9b6139a9fbf2431015e395
CarlsFencingDecking <158094575+CarlsFencingDecking@users.noreply.github.com>
2025-06-13 11:13:23 -0400

    Update editor.js
    add full function

--- js/editor.js
+++ js/editor.js
@@ -270,7 +270,53 @@ MD.Editor = function(){
   window.addEventListener('message', function(event) {
     if (event.data && event.data.type === 'saveImage') {
         console.log('received from netsuite')
-        exportHandler()
+        function exportHandler(window, data) {
+          //var issues = data.issues;
+      
+          if (!$('#export_canvas').length) {
+              $('<canvas>', { id: 'export_canvas' }).hide().appendTo('body');
+          }
+          var c = $('#export_canvas')[0];
+      
+          c.width = svgCanvas.contentW;
+          c.height = svgCanvas.contentH;
+      
+          canvg(c, data.svg, {
+              renderCallback: function () {
+                  var datauri = c.toDataURL('image/png');
+                  if (!datauri) return false;
+      
+                  // ✅ Send the base64 PNG data to parent frame (your Suitelet)
+                  window.parent.postMessage({
+                      type: 'exported_png',
+                      pngDataUri: datauri
+                  }, '*');
+      
+                /*
+      
+                  var filename = "Method Draw Image";
+                  var type = 'image/png';
+                  var file = svgedit.utilities.dataURItoBlob(datauri, type);
+      
+                  if (window.navigator.msSaveOrOpenBlob) {
+                      window.navigator.msSaveOrOpenBlob(file, filename);
+                  } else {
+                      var a = document.createElement("a"),
+                          url = URL.createObjectURL(file);
+                      a.href = url;
+                      a.download = filename;
+                      document.body.appendChild(a);
+                      a.click();
+                      setTimeout(function () {
+                          document.body.removeChild(a);
+                          window.URL.revokeObjectURL(url);
+                      }, 0);
+                  }
+      
+                  */
+              }
+          });
+        }
       
     }
   });
