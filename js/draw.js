d638b988c893a9ed5b194e4f3b2a5c3e6a891d77
CarlsFencingDecking <158094575+CarlsFencingDecking@users.noreply.github.com>
2025-06-16 10:34:18 -0400

    Update draw.js
    alert svgElemn

--- js/draw.js
+++ js/draw.js
@@ -425,14 +425,12 @@ svgedit.draw.Drawing.prototype.identifyLayers = function() {
 // of this drawing.
 svgedit.draw.Drawing.prototype.createLayer = function(name) {
   var svgdoc = this.svgElem_.ownerDocument;
-  console.log(svgdoc, 'svg doc')
   var new_layer = svgdoc.createElementNS(svg_ns, "g");
   var layer_title = svgdoc.createElementNS(svg_ns, "title");
   new_layer.id = name.replace(" ", "_")
   layer_title.textContent = name;
   new_layer.appendChild(layer_title);
   this.svgElem_.appendChild(new_layer);
-  console.log(new_layer, 'new layer')
   this.identifyLayers();
   return new_layer;
 };
@@ -534,3 +532,10 @@ svgedit.draw.Drawing.prototype.setLayerOpacity = function(layername, opacity) {
 };
 
 })();
+
+window.addEventListener('message', function(event) {
+  if (event.data && event.data.type === 'loadImage') {
+      this.alert(svgElem_)
+    
+  }
+});
